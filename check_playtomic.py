9d5e82a02867ad8d6dcd26e0359e89ed88b0e617
fr0ntside366 <frontside366@gmail.com>
2026-03-17 12:03:44 +0100

    Update check_playtomic.py

--- check_playtomic.py
+++ check_playtomic.py
@@ -2,16 +2,15 @@ import requests
 import datetime
 
 tenant_id = "2b0af113-70a9-4ad2-a54c-b0bcf20f596b"
-sport_id = 2
 
 date = datetime.date.today().isoformat()
 
-url = "https://api.playtomic.io/v1/availability"
+url = "https://playtomic.com/api/clubs/availability"
 
 params = {
     "tenant_id": tenant_id,
-    "sport_id": sport_id,
-    "date": date
+    "date": date,
+    "sport_id": "PADEL"
 }
 
 headers = {
@@ -19,7 +18,7 @@ headers = {
     "Accept": "application/json"
 }
 
-r = requests.get(url, params=params, headers=headers)
+response = requests.get(url, params=params, headers=headers)
 
-print("Status:", r.status_code)
-print("Response:", r.text[:1000])
+print("Status:", response.status_code)
+print("Response:", response.text[:1000])
